ad55e54c4360629066bb4f720c09c7373ddc5fcb
Matthew Berryman <matthew@acrossthecloud.net>
2021-07-06 14:40:59 +1000

    add a flag for filtering on tag value

--- ec2-ip-addresses.go
+++ ec2-ip-addresses.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 )
 
 func main() {
+
+	tagPtr := flag.String("tag", "", "ec2-instance-tag-value")
+	flag.Parse()
+
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
 		panic("configuration error, " + err.Error())
@@ -17,6 +24,19 @@ func main() {
 	client := ec2.NewFromConfig(cfg)
 
 	input := &ec2.DescribeInstancesInput{}
+
+	if *tagPtr != "" {
+		input = &ec2.DescribeInstancesInput{
+			Filters: []types.Filter{
+				{
+					Name: aws.String("tag-value"),
+					Values: []string{
+						*tagPtr,
+					},
+				},
+			},
+		}
+	}
 
 	result, err := client.DescribeInstances(context.TODO(), input)
 	if err != nil {
