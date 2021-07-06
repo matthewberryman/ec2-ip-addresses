3767e3133988b77c2c5a05c763d19acc54292986
Matthew Berryman <matthew@acrossthecloud.net>
2021-07-06 13:31:47 +1000

    initial cut

---
+++ ec2-ip-addresses.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+)
+
+func main() {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic("configuration error, " + err.Error())
+	}
+
+	client := ec2.NewFromConfig(cfg)
+
+	input := &ec2.DescribeInstancesInput{}
+
+	result, err := client.DescribeInstances(context.TODO(), input)
+	if err != nil {
+		fmt.Println("Got an error retrieving information about your Amazon EC2 instances:")
+		fmt.Println(err)
+		return
+	}
+
+	for _, r := range result.Reservations {
+		fmt.Println("Reservation ID: " + *r.ReservationId)
+		fmt.Println("Instance IDs:")
+		for _, i := range r.Instances {
+			fmt.Println("   " + *i.InstanceId)
+			fmt.Println("   " + *i.NetworkInterfaces[len(i.NetworkInterfaces)-1].Association.PublicDnsName)
+			fmt.Println("   " + *i.NetworkInterfaces[len(i.NetworkInterfaces)-1].Association.PublicIp)
+		}
+
+		fmt.Println("")
+	}
+}
